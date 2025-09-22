4caeaa39ba6e746a94e95e72c39028bfdd2c2bd0
Joe Chung <joechung2008@users.noreply.github.com>
2025-09-22 19:41:05 -0400

    Fix default port for dart_frog server.dart

--- packages/dart_frog_app/bin/server.dart
+++ packages/dart_frog_app/bin/server.dart
@@ -3,7 +3,7 @@ import 'package:dart_frog_app/server.dart' as server_lib;
 
 Future<void> main(List<String> args) async {
   final ip = InternetAddress.anyIPv4;
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final port = int.parse(Platform.environment['PORT'] ?? '8000');
   final server = await server_lib.startServer(address: ip, port: port);
   print('Dart Frog server listening on port ${server.port}');
 }
